24672cac53384e5a3628657c2d8b8914caa263d9
Gabriel Britain <gabriel.britain@protonmail.com>
2022-06-29 23:01:19 -0500

    Added csv concatenation

--- src/main.py
+++ src/main.py
@@ -1,16 +1,22 @@
 import csv
 import os
 from concurrent.futures import ProcessPoolExecutor
-from typing import List, Tuple
+import pathlib
+import re
+from typing import Dict, List, Tuple
 
 import bs4
 import requests
+import urllib3
 from requests import Response
+import pandas as pd
 
 import pdf_parser
 
+urllib3.disable_warnings()
+
 ROOT_URL = "https://web-as.tamu.edu/GradeReports/"
-PDF_URL = "https://web-as.tamu.edu/gradereport/PDFReports/{}/grd{}{}.pdf"
+PDF_URL = "https://web-as.tamu.edu/GradeReports/PDFReports/{}/grd{}{}.pdf"
 PDF_DOWNLOAD_DIR = os.path.abspath("documents/pdfs")
 
 SPRING, SUMMER, FALL = "1", "2", "3"
@@ -59,6 +65,7 @@ def download_pdf(args: Tuple[str, str]) -> str:
     response = requests.get(url, verify=False)
     try:
         response.raise_for_status()
+        print(year_semester, college)
         with open(path, "wb+") as file:
             file.write(response.content)
             return path
@@ -75,8 +82,9 @@ def get_files_in_dir(directory: str, extension: str) -> List[str]:
     return abspaths
 
 
-def main():
+def parse_csvs():
     yrs = years()
+    print(yrs)
     college_abbrevs = colleges()
     arguments = []
     for year in yrs:
@@ -112,11 +120,56 @@ def main():
                     ]
                 )
                 for dist in distribution_fields:
-                    (grades, (dept, course_num, section_num, instructor_name),) = dist
+                    (
+                        grades,
+                        (dept, course_num, section_num, instructor_name),
+                    ) = dist
                     writer.writerow(
                         [*grades, dept, course_num, section_num, instructor_name]
                     )
 
 
+def grd_to_term_code(grd: str) -> str:
+    try:
+        [(year, semester, college)] = re.findall(
+            "grd(?P<year>\d{4})(?P<semester>\d)(?P<college>[\w\_]+)", grd
+        )
+        if college == "GV":
+            return year + semester + "2"
+        if college == "QT":
+            return year + semester + "3"
+        return year + semester + "1"
+    except Exception as e:
+        print(grd)
+        raise e
+
+
+def concat_csvs():
+    paths = sorted(
+        [
+            pathlib.Path(abspath)
+            for abspath in get_files_in_dir(PDF_DOWNLOAD_DIR, ".csv")
+        ]
+    )
+    dataframes: Dict[str, pd.DataFrame] = {}
+    for path in paths:
+        term_code = grd_to_term_code(str(path))
+        df = pd.read_csv(path)
+        if term_code not in dataframes:
+            dataframes[term_code] = df
+        else:
+            dataframes[term_code] = pd.concat([dataframes[term_code], df])
+    for key, val in dataframes.items():
+        val.to_csv(os.path.join("documents", "csvs", f"{key}.csv"))
+
+
+def main():
+    parse_csvs()
+    concat_csvs()
+
+
 if __name__ == "__main__":
     main()
+
+# https://web-as.tamu.edu/gradereport/PDFReports/20211/grd20211EN.pdf
+# https://web-as.tamu.edu/GradeReports/PDFReports/20211/grd20211EN.pdf
